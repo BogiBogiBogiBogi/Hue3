9990fe92dfc9cf7dd8b7ec718b8b05bb9318f772
BogiBogiBogiBogi <bogner.htlgrk@gmail.com>
2020-10-05 14:07:01 +0200

    version2

--- src/hue3/WeaponsReader.java
+++ src/hue3/WeaponsReader.java
@@ -9,21 +9,27 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class WeaponsReader {
-    ArrayList<Weapons> list = new ArrayList<Weapons>();
 
     public WeaponsReader() {
-        list = new ArrayList();
     }
-    
-    public void readFile()
-    {
-        
+
+    public ArrayList<Weapons> readFile() {
+        ArrayList<Weapons> list = new ArrayList<Weapons>();
+
         try {
             File file = new File("weapons.csv");
             Scanner scan = new Scanner(file);
-            
+            String s = "";
+            while(scan.hasNext()){
+                s = scan.nextLine();
+                String array[] = s.split(";");
+                Weapons w;
+                w = new Weapons(CombatType.valueOf(array[1].toUpperCase()), DamageType.valueOf(array[2].toUpperCase()), array[0], Integer.parseInt(array[3]), Integer.parseInt(array[4]), Integer.parseInt(array[5]), Integer.parseInt(array[6]));
+                list.add(w);
+            }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(WeaponsReader.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return list;
     }
 }
